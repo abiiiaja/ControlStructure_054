571f9d678330a4c0108c731091e89bc2d445680c
abiiiaja <kalindraaraya@gmail.com>
2026-09-22 11:17:04 +0700

    revisi kalimat pada soal nomor 4

--- nomor_4.py
+++ nomor_4.py
@@ -1,3 +1,3 @@
-n_ganjil = int(input("Masukkan berapa banyak bilangan ganjil yang ingin ditampilkan: "))
+n_ganjil = int(input("Masukkan angka ganjil terbesar pada bilangan ganjil yang ingin ditampilkan: "))
 for n in range(1, n_ganjil + 1, 2):
     print(n, end=" ")
