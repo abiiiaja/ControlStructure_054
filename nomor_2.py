8e80be02cfa172853fe67d3a48db1a5f7beabe13
abiiiaja <kalindraaraya@gmail.com>
2026-09-22 10:24:37 +0700

    ketiga angka disimpan di list

--- nomor_2.py
+++ nomor_2.py
@@ -1,3 +1,4 @@
 angka_1 = int(input("Masukkan angka pertama"))
 angka_2 = int(input("Masukkan angka kedua"))
 angka_3 = int(input("Masukkan angka ketiga"))
+kumpulan_angka = [angka_1, angka_2, angka_3]
