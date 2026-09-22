d2e9716dc86fee16824a8a0f5c8f13fb14bc07a1
abiiiaja <kalindraaraya@gmail.com>
2026-09-22 10:26:06 +0700

    jawaban final nomor 2

--- nomor_2.py
+++ nomor_2.py
@@ -1,4 +1,6 @@
-angka_1 = int(input("Masukkan angka pertama"))
-angka_2 = int(input("Masukkan angka kedua"))
-angka_3 = int(input("Masukkan angka ketiga"))
+angka_1 = int(input("Masukkan angka pertama: "))
+angka_2 = int(input("Masukkan angka kedua: "))
+angka_3 = int(input("Masukkan angka ketiga: "))
 kumpulan_angka = [angka_1, angka_2, angka_3]
+angka_terbesar = max(kumpulan_angka)
+print(f"Angka terbesar adalah: {angka_terbesar}")
