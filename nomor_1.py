a4ea02a0e1f5565408368d50ef7c54c8389f4ab7
abiiiaja <kalindraaraya@gmail.com>
2026-09-22 10:21:17 +0700

    Kodingan jawaban nomor 1

---
+++ nomor_1.py
@@ -0,0 +1,10 @@
+performa = int(input("Masukkan performa Anda: "))
+if performa >= 90:
+    print("Excellent performance")
+elif performa >= 80:
+    print("Very Good performance")
+elif performa >= 70:
+    print("Good performance")
+elif performa >= 60:
+    print("Average performance")
+
