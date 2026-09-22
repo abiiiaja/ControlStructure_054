897c57a34d3867129fa628371a2e524b037291cb
abiiiaja <kalindraaraya@gmail.com>
2026-09-22 11:02:31 +0700

    revisi soal nomor 2

--- nomor_2.py
+++ nomor_2.py
@@ -1,6 +1,11 @@
 angka_1 = int(input("Masukkan angka pertama: "))
 angka_2 = int(input("Masukkan angka kedua: "))
 angka_3 = int(input("Masukkan angka ketiga: "))
-kumpulan_angka = [angka_1, angka_2, angka_3]
-angka_terbesar = max(kumpulan_angka)
-print(f"Angka terbesar adalah: {angka_terbesar}")
+if angka_1 >= angka_2 and angka_1 >= angka_3:
+    print(f"Angka terbesar adalah: {angka_1}")
+elif angka_2 >= angka_1 and angka_1 >= angka_3:
+    print(f"Angka terbesar adalah: {angka_2}")
+elif angka_3 >= angka_1 and angka_3 >= angka_1:
+    print(f"Angka terbesar adalah: {angka_3}")
+else:
+    print("Seri")
