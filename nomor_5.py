4a85d92e5b35f7d1a3fb3046f74f10d8c3ea9917
abiiiaja <kalindraaraya@gmail.com>
2026-09-22 10:40:40 +0700

    revisi jawaban jika n bisa semau user

--- nomor_5.py
+++ nomor_5.py
@@ -1,3 +1,3 @@
 n_segitiga = int(input("Masukkan berapa banyak bilangan pada segitiga: "))
 for n in range(1, n_segitiga + 1):
-    print(f" 5{n}" * n, end="\n\n")
+    print(f" {n}" * n, end="\n\n")
