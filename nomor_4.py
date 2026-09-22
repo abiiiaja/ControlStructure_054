68b0440b0de77ec34d8b054cef77cc5c5f13f17a
abiiiaja <kalindraaraya@gmail.com>
2026-09-22 10:35:40 +0700

    revisi jawaban soal nomor 4

--- nomor_4.py
+++ nomor_4.py
@@ -1,3 +1,3 @@
 n_ganjil = int(input("Masukkan berapa banyak bilangan ganjil yang ingin ditampilkan: "))
-for n in range(1, n_ganjil * 2, 2):
+for n in range(1, n_ganjil + 1, 2):
     print(n, end=" ")
